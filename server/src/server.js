c7c3df9aa1067fc18b5849161ad9d9c7f71e2f29
jamaliumair <jamaliumair957@gmail.com>
2026-05-08 15:39:22 +0500

    db codebase setup

--- server/src/server.js
+++ server/src/server.js
@@ -2,10 +2,21 @@ import express from 'express';
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
 
-connectDB();
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => console.log('Server is running on port', ENV.PORT));
+    }
+    catch (error) {
+        console.error('Failed to connect to the database:', error);
+        process.exit(1);
+    }
+}
+
 
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(ENV.PORT, () => console.log('Server is running on port', ENV.PORT)); ;
+startServer();
+
